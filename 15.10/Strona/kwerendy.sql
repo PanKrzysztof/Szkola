83885d8ded5d3e6033e71132401e65160242ffa3
PanKrzysztof <krzysztof.holeczko213@gmail.com>
2024-11-28 14:33:35 +0100

    usuwanie

--- 15.10/Strona/kwerendy.sql
+++
@@ -1,4 +0,0 @@
-SELECT miejsce, liczbaDni FROM wycieczki WHERE cena<1000;
-SELECT AVG(cena) sredniaCena FROM wycieczki GROUP BY liczbaDni;
-SELECT miejsce, nazwa FROM zdjecia JOIN wycieczki ON zdjecia.Wycieczki_id=wycieczki.id WHERE cena>500;
-CREATE USER 'EWA'@'localhost' identyfied BY 'EWA!EWA'
