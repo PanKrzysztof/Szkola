976e2df6bff099ee1b20009a2166fb30881ffb83
PanKrzysztof <krzysztof.holeczko213@gmail.com>
2024-11-29 10:40:37 +0100

    Strona 29.11

---
+++ 29.11/Strona/JS/script.js
@@ -0,0 +1,15 @@
+function cytat1()
+{
+    document.getElementById("cytat1").style.display = "none";
+    document.getElementById("cytat2").style.display = "block";
+}
+function cytat2()
+{
+    document.getElementById("cytat2").style.display = "none";
+    document.getElementById("cytat3").style.display = "block";
+}
+function cytat3()
+{
+    document.getElementById("cytat3").style.display = "none";
+    document.getElementById("cytat1").style.display = "block";
+}
