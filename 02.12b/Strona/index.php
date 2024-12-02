2f72d56eecb7c3c14311352190c4a675798a0a17
PanKrzysztof <krzysztof.holeczko213@gmail.com>
2024-12-02 10:16:57 +0100

    Strona 02.12b

---
+++ 02.12b/Strona/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sklep papierniczy</title>
+    <link rel="stylesheet" href="CSS/styl.css">
+</head>
+<body>
+    
+</body>
+</html>
